a16a49d04e432f286e2a09b13766f71ea8665078
Ana Beatriz Oliveira <ana.bo22@aluno.ifsc.edu.br>
2022-02-18 16:08:21 -0300

    Add project files.

---
+++ FrequenciaDeNumeros.ConsoleApp/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Linq;
+
+namespace FrequenciaDeNumeros.ConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int quantDeValores;
+            
+            Console.WriteLine("Digite a quantidade de valores que deseja ler: ");
+            quantDeValores = Convert.ToInt32(Console.ReadLine());
+            int[] valores = new int[quantDeValores];
+            
+
+            for (int i = 0; i < valores.Length; i++)
+            {
+                Console.WriteLine("Digite o valor: ");
+                valores[i] = Convert.ToInt32(Console.ReadLine());
+            }
+            int contador = 0;
+       
+
+            for(int i = 0;i < valores.Length; i++)
+            {
+                contador = 0;
+                for (int j = 0; j < valores.Length; j++)
+                {
+                    if (valores[i] == valores[j])
+                    {
+                        contador++;
+
+                    }
+                   
+                }
+              
+                Console.WriteLine("{0} aparece {1} vezes", valores[i], contador);
+            }
+            Console.ReadLine();
+        }
+    }
+}
